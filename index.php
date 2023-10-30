b19401407f6ada2f8209ee23285da565649e2909
trupen1010 <adrojatrupen@gmail.com>
2023-10-30 08:23:58 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,282 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Trupen Adroja</title>
+
+    <link rel="shortcut icon" href="./assets/img/trupen-adroja.jpg" />
+
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:200,300,400,500,600,700,800" rel="stylesheet">
+
+    <link rel="stylesheet" href="./assets/css/plugins.css" />
+
+    <link rel="stylesheet" href="./assets/css/style.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body class="theme-dark">
+    <div class="loading">
+        <div class="text-center middle">
+            <div class="lds-ellipsis">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    </div>
+    <nav class="navbar navbar-expand-lg">
+        <div class="container">
+            <a class="logo" href="#">
+                Trupen Adroja
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="icon-bar"><i class="fas fa-bars"></i></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#" data-scroll-nav="0">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" data-scroll-nav="1">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" data-scroll-nav="2">Skills</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <header class="header valign bg-img" data-overlay-dark="6" style="background-image: url('./assets/img/background.jpg')">
+        <div class="svg">
+            <svg x="0px" y="0px" viewBox="0 186.5 1920 113.5">
+                <polygon points="-30,300 355.167,210.5 1432.5,290 1920,198.5 1920,300"></polygon>
+            </svg>
+        </div>
+
+        <div class="container">
+            <div class="row">
+                <div class="full-width text-center caption mt-30">
+                    <h1 class="cd-headline clip">
+                        <span>I Am </span>
+                        <span class="cd-words-wrapper">
+                            <b class="is-visible">Trupen Adroja</b>
+                            <b>Web Developer</b>
+                            <b>Full Stack Developer</b>
+                        </span>
+                    </h1>
+                </div>
+            </div>
+        </div>
+
+        <div class="arrow bounce" data-scroll-nav="1">
+            <img src="./assets/img/down-chevron.svg" alt="">
+        </div>
+
+    </header>
+    <section class="hero section-padding" data-scroll-index="1">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-lg-5">
+                    <div class="author-img">
+                        <img src="./assets/img/trupen-adroja.jpg" alt="">
+                    </div>
+                </div>
+
+                <div class="col-lg-7">
+                    <div class="content">
+                        <h4>I&#039;m Trupen Adroja</h4>
+                        <p>Successfully contributed to the development and maintenance of web applications using PHP (CodeIgniter and Laravel), ensuring high performance, security, and user-friendliness.</p>
+                        <p>Implemented Object-Oriented Programming (OOP) concepts and Model-View-Controller (MVC) architecture to create scalable and maintainable codebases.</p>
+                        <p>Collaborated with cross-functional teams to design and develop database structures and optimize data retrieval using MySQL.</p>
+                        <p>Utilized Git for version control, ensuring efficient code collaboration and management with the development team.</p>
+                        <p>Demonstrated expertise in web development, adhering to responsive web design principles to deliver optimal user experiences.</p>
+                        <p>Applied knowledge of JavaScript, jQuery, Bootstrap, HTML5, and CSS3 to create interactive and visually appealing front-end features.</p>
+                        <p>Participated in e-commerce projects, contributing to the development of online shopping platforms and payment gateways.</p>
+                        <p>Actively engaged in code reviews and quality assurance processes to maintain high coding standards.</p>
+                        <p>Assisted in the integration of third-party APIs, enhancing the functionality and capabilities of web applications.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="hero section-padding" data-scroll-index="2">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h4>Skills</h4>
+                </div>
+                <div class="col-lg-6">
+                    <div class="skills mt-50">
+                        <div class="skill-item">
+                            <h6>Laravel
+                                <span>80</span>
+                            </h6>
+                            <div class="skills-progress"><span data-value="80%"></span></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="skills mt-50">
+                        <div class="skill-item">
+                            <h6>CodeIgniter
+                                <span>60</span>
+                            </h6>
+                            <div class="skills-progress"><span data-value="60%"></span></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="skills mt-50">
+                        <div class="skill-item">
+                            <h6>Object-Oriented Programming (OOP)
+                                <span>60</span>
+                            </h6>
+                            <div class="skills-progress"><span data-value="60%"></span></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="skills mt-50">
+                        <div class="skill-item">
+                            <h6>Model-View-Controller (MVC)
+                                <span>80</span>
+                            </h6>
+                            <div class="skills-progress"><span data-value="80%"></span></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="skills mt-50">
+                        <div class="skill-item">
+                            <h6>Database Design
+                                <span>80</span>
+                            </h6>
+                            <div class="skills-progress"><span data-value="80%"></span></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="skills mt-50">
+                        <div class="skill-item">
+                            <h6>Web Application Development
+                                <span>80</span>
+                            </h6>
+                            <div class="skills-progress"><span data-value="80%"></span></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="skills mt-50">
+                        <div class="skill-item">
+                            <h6>Back-End Web Development
+                                <span>80</span>
+                            </h6>
+                            <div class="skills-progress"><span data-value="80%"></span></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="skills mt-50">
+                        <div class="skill-item">
+                            <h6>Responsive Web Design
+                                <span>60</span>
+                            </h6>
+                            <div class="skills-progress"><span data-value="60%"></span></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="skills mt-50">
+                        <div class="skill-item">
+                            <h6>Application Programming Interfaces (API)
+                                <span>80</span>
+                            </h6>
+                            <div class="skills-progress"><span data-value="80%"></span></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="skills mt-50">
+                        <div class="skill-item">
+                            <h6>PhpMyAdmin
+                                <span>80</span>
+                            </h6>
+                            <div class="skills-progress"><span data-value="80%"></span></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="skills mt-50">
+                        <div class="skill-item">
+                            <h6>MERN Stack (MongoDB, Express.js, React.js, Node.js)
+                                <span>40</span>
+                            </h6>
+                            <div class="skills-progress"><span data-value="40%"></span></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="skills mt-50">
+                        <div class="skill-item">
+                            <h6>GitHub
+                                <span>40</span>
+                            </h6>
+                            <div class="skills-progress"><span data-value="40%"></span></div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <div class="se-tring se-tring-gray">
+            <span class="left"></span>
+            <span class="right"></span>
+        </div>
+    </section>
+
+    <footer class="text-center">
+        <div class="container">
+            <a class="logo" href="#">
+                Trupen Adroja
+            </a>
+            <div class="social">
+                <a href="https://www.linkedin.com/in/trupen-adroja"><i class="fab fa-linkedin"></i></a>
+            </div>
+        </div>
+    </footer>
+
+    <script src="./assets/js/jquery-3.0.0.min.js"></script>
+    <script src="./assets/js/jquery-migrate-3.0.0.min.js"></script>
+
+    <script src="./assets/js/popper.min.js"></script>
+
+    <script src="./assets/js/bootstrap.min.js"></script>
+
+    <script src="./assets/js/scrollIt.min.js"></script>
+
+    <script src="./assets/js/owl.carousel.min.js"></script>
+
+    <script src="./assets/js/animated.headline.js"></script>
+
+    <script src="./assets/js/jquery.magnific-popup.min.js"></script>
+
+    <script src="./assets/js/jquery.stellar.min.js"></script>
+
+    <script src="./assets/js/isotope.pkgd.min.js"></script>
+
+    <script src="./assets/js/validator.js"></script>
+
+    <script src="./assets/js/scripts.js"></script>
+</body>
+
+</html>
